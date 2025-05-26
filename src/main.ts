b3d204d348b7c764bed00350f7a7e221a2044347
Mykola Gurov <ngurov@gmail.com>
2025-05-26 20:34:46 +0200

    experimentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { animate, utils, createDraggable, createSpring } from 'animejs';
+import { animate, utils, createDraggable, createSpring, createTimer } from 'animejs';
 
 const [ $logo ] = utils.$('.logo.js');
 const [ $button ] = utils.$('button');
 let rotations = 0;
+
+// the timer
+const [ $time, $count ] = utils.$('.value');
+
+createTimer({
+  duration: 1000,
+  loop: true,
+  frameRate: 30,
+  onUpdate: self => $time.innerHTML = self.currentTime,
+  onLoop: self => $count.innerHTML = self._currentIteration
+});
 
 // Created a bounce animation loop
 animate('.logo.js', {
